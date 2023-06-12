2dd02bbdfc6db0d72cfcbf33d13e503b11300412
Pedro Lima <pedro.oficial1969@gmail.com>
2023-06-12 14:43:26 -0300

    feat(install): instalação do react-icons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="bg-zinc-100 w-full h-full absolute flex items-center justify-center">
-      <div className="bg-zinc-300 w-[162px] h-[90px] rounded-[70px] shadow-[inset_5px_0px_15px_-3px_rgba(0,0,0,0.25)] flex items-center">
+      <div className="bg-zinc-300 w-[162px] h-[90px] rounded-[70px] shadow-[inset_5px_0px_15px_-3px_rgba(0,0,0,0.25)] py-[5px] px-[6px] flex justify-end cursor-pointer">
         <div className="bg-zinc-100 w-[80px] h-[80px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-full"></div>
       </div>
     </div>
